af2da47b7e3f91a5f379ee0f5f9de8f4f31c0aae
yıldız <mesel4461@gmail.com>
2023-07-13 23:41:20 +0300

    as

--- src/components/AddPostForm.jsx
+++ src/components/AddPostForm.jsx
@@ -1,4 +1,27 @@
 import React from "react";
+import {
+  Buttom,
+  TextField,
+  Select,
+  Input,
+  MenuItem,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
+import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers";
+
+const tags = ["fun", "programming", "health", "science"];
+
+const postSchema = yup.object().shape({
+  title: yup.string().required(),
+  subtitle: yup.string().required(),
+  content: yup.string().min(20).required(),
+});
 
 const AddPostForm = ({ open, setClose }) => {
   return <div>AddPostForm</div>;
